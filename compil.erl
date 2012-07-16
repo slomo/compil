7a5ee5bb1fa7e04679c023072522bcdd3828137b
yves <steve.harrison@gmx.net>
2012-07-16 14:58:27 +0200

    basic let handling

--- compil.erl
+++ compil.erl
@@ -1,5 +1,5 @@
 %% -----------------------------------------------------------------------------
-%% @author Yves Müller <myves*addAnAt*zedat.fu-berlin.de>
+%% @author Yves Müller <myves.addAnAt.zedat.fu-berlin.de>
 %% @copyright Simplified BSD License by Yves Müller, 2012
 %% @title Compil.erl
 %% @doc A simple compiler for a lispy language
@@ -11,37 +11,49 @@
 
 %% @doc entrypoint for compilation
 do(String) ->
-    compil_table:start_link(),
-    {ok,Tokens,_Lines} = lfe_scan:string(String),
-    {ok,1,Tree,_Cont} = lfe_parse:sexpr(Tokens),
-    Phase2 = entryPoint(phase2(Tree)),
-    io:write(Phase2),
-    io:fwrite("~n---------~n"),
-    {_type,Typed} = type(Phase2),
-    io:write(Typed),
-    io:fwrite("~n---------~n"),
-    {_var,PreCode} = codegen(Typed),
-    io:write(PreCode),
-    io:fwrite("~n---------~n"),
-    io:fwrite(lists:foldr(
-                fun(A,B) ->
-                        case A of
-                            "" -> B;
-                            A -> A ++ " \n" ++ B
-                                     end
-                end,"", PreCode)).
+    {ok, _Pid} = compil_table:start_link(),
+    {ok, Tokens, _Lines} = lfe_scan:string(String),
+    {ok, _Count, Tree, _Cont} = lfe_parse:sexpr(Tokens),
 
--type primOp() :: '+' | '-' | '*' | '/'.
--type systemType() :: int | float | bool.
--type from() :: #primOp | #literal.
+    addGlobalEnv(),
+    EnvCode = split_env(Tree, global),
+    NestingFreeCode = clear_names(EnvCode),
+    compil_table:stop(),
+    NestingFreeCode.
+
+
+
+
+addGlobalEnv() ->
+    compil_table:newEnv(global, undefined).
+
+
+%    Phase2 = entryPoint(phase2(Tree)),
+%    io:write(Phase2),
+%    io:fwrite("~n---------~n"),
+%    {_type,Typed} = type(Phase2),
+%    io:write(Typed),
+%    io:fwrite("~n---------~n"),
+%    {_var,PreCode} = codegen(Typed),
+%    io:write(PreCode),
+%    io:fwrite("~n---------~n"),
+%    io:fwrite(lists:foldr(
+%                fun(A,B) ->
+%                        case A of
+%                            "" -> B;
+%                            A -> A ++ " \n" ++ B
+%                                     end
+%                end,"", PreCode)).
+
 -define(PRIMOPS,['+','-','*','/']).
+-define(BUILDIN,?PRIMOPS).
 
 
 
 -record(primOp,{
-          op :: primeOp(),
-          type :: systemType()
-          operands :: {#form(), #form()}
+          op :: #primOp{},
+          type :: systemType(),
+          operands :: {form(), form()}
          }).
 -record(literal,{
           type :: systemType(),
@@ -49,14 +61,82 @@ do(String) ->
          }).
 -record(entry,{
           type :: systemType(),
-          form :: primOp | record 
+          form :: primOp | record
          }).
 
+-record(letForm,{
+         type :: systemType(),
+         bindings :: [{atom(), string(), form()}],
+         form :: form()
+        }).
+
+-type primOp() :: '+' | '-' | '*' | '/'.
+-type systemType() :: int | float | bool.
+-type form() :: #primOp{} | #literal{}.
+
+-record(name,{
+	  env :: reference(),
+	  name :: string(),
+	  oldName :: atom()
+	 }).
+
+-record(env,{
+	  form :: 'let' | 'lambda',
+	  ref  :: reference(),
+	  parent,
+	  definitions,
+	  childs
+	 }).
+
+-record(call,{
+	  function,
+	  arguments
+	 }).
+
+
+
+split_env([ 'lambda' | [Bindings | Child]], OldEnv) ->
+    Ref = make_ref(),
+    compil_table:newEnv(Ref, OldEnv),
+    ScannedDef = lists:map( fun
+            ([Var, Forms]) ->
+                [split_env(Var, Ref), split_env(Forms, Ref)]
+        end, Bindings),
+    ScannedChild = split_env(Child, Ref),
+    #env{ form = 'let', ref = Ref, childs = [ScannedChild],definitions = ScannedDef };
+
+split_env([ Function | Arguments], Env) ->
+    FunctionName = split_env(Function, Env),
+    ScannedArguments = lists:map(fun (Thing) ->  split_env(Thing,Env) end, Arguments),
+    #call{ function = FunctionName, arguments = ScannedArguments};
+
+split_env(Atom, _Env) when is_atom(Atom) ->
+    #name{ oldName = Atom};
+
+%split_env(StringLit) when is_string(StringLit) ->
+%    #literal{ type=string, value=StringLit };
+split_env(IntLit, _Env) when is_integer(IntLit) ->
+    #literal{ type=int, value=IntLit };
+split_env(BoolLit, _Env) when is_boolean(BoolLit) ->
+    #literal{ type=bool, value=BoolLit}.
+
+
+clear_names(
+
+
+%phase2([ 'let' | [Bindings | Form]]) ->
+%    Definitons = lists:map(fun ([ Name, Form ]) when is_atom(Def) ->
+%                NewName = compil_table:newUnique(Def),
+%                {{Name, NewName}, Form} end, Bindings),
+
+
+
+
 phase2([Head | Forms]) ->
-    case lists:member(Head,?PRIMOPS) of 
+    case lists:member(Head,?PRIMOPS) of
         true ->
             foldPrimOp(Head, Forms);
-        false -> 
+        false ->
             io:write({error,{unknowExpressionType,Head}})
     end;
 phase2(Lit) when is_integer(Lit) ->
@@ -80,7 +160,7 @@ type(Expr) ->
             MyType = maxType(TypeA,TypeB),
             {MyType,Expr#primOp{type=MyType,operands={NewA,NewB}}};
         #literal{type=Type} ->
-            {Type,Expr};                 
+            {Type,Expr};
         #entry{form=Form} ->
             {MyType,NewForm} = type(Form),
             {MyType,Expr#entry{form=NewForm,type=MyType}}
@@ -88,8 +168,6 @@ type(Expr) ->
 
 maxType(int,int) ->
     int.
-    
-            
 
 % code gen for the known  type
 
@@ -111,7 +189,6 @@ codegen(Expr) ->
             TypeStr = decodeType(Type),
             MyCode = [ "define " ++ TypeStr ++ " @main () {" ]  ++ Code ++ [ "ret " ++ TypeStr ++ " " ++ Var ++ "\n}"],
             {"main", MyCode}
-   
     end.
 
 
--- compil_table.erl
+++ compil_table.erl
@@ -1,40 +1,85 @@
+%% -----------------------------------------------------------------------------
+%% @author Yves Müller <myves.addAnAt.zedat.fu-berlin.de>
+%% @copyright Simplified BSD License by Yves Müller, 2012
+%% @title Compil.erl
+%% @doc A table server, for supporting the Compil compiler
+%% @version 0.0.1
+%% -----------------------------------------------------------------------------
+
+
 -module(compil_table).
 
 -behaviour(gen_server).
 
--export([start_link/0,stop/0]).
+-export([start_link/0, stop/0]).
 -export([init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2, code_change/3]).
+
+
+-define(RENAME_TABLE,compil_rename_table).
+-define(ENV_TABLE,compil_env_table).
+
+% -- Entries -------------
+
+-record(envEntry,{ ref, parent}).
 
 % -- API -----------
 
--export([newTemp/0]).
+-export([newTemp/0, newEnv/2, getParentEnv/1]).
 
 newTemp() ->
-    gen_server:call(?MODULE,newTemp).
+    gen_server:call(?MODULE, newTemp).
+
+newUnique(Atom) ->
+    gen_server:call(?MODULE, {newUnique, Atom}).
+
+
+newEnv(Ref,Parent) ->
+    gen_server:call(?MODULE, {store, ?ENV_TABLE, #envEntry{ref = Ref, parent = Parent}}).
+
+getParentEnv(Ref) ->
+    #envEntry{ parent = Parent } = gen_server:call(?MODULE, {get, ?ENV_TABLE, Ref}),
+    Parent.
 
 
 % -- Internals -----
 
 -record(state,{counter,rename}).
 
--define(RENAME_TABLE,compil_rename_table).
-
 start_link() ->
     gen_server:start({local, ?MODULE} ,?MODULE ,[] ,[]).
+%    case whereis(?MODULE) of
+%        Pid when is_pid(Pid) ->
+%            {ok, Pid};
+%        undefined ->
+%            gen_server:start({local, ?MODULE} ,?MODULE ,[] ,[])
+%    end.
 
 stop() ->
     gen_server:cast(?MODULE, stop).
 
 init(_Args)->
     Table = ets:new(?RENAME_TABLE, [set,protected,named_table]),
+    ets:new(?ENV_TABLE, [set,protected,named_table,{keypos,2}]),
     {ok,#state{rename=Table,counter=0}}.
 
-handle_call(Msg, _From, State = #state{counter=C, rename=_RTable}) ->
+handle_call(Msg, _From, State = #state{counter=C, rename=RTable}) ->
     case Msg of
         newTemp ->
             {reply, "%tmp" ++ integer_to_list(C), State#state{counter=C+1}};
+        {newUnique,Atom} ->
+            case ets:lookup(RTable, Atom) of
+                [{Atom, Counter}] ->
+                    ets:insert(RTable,{Atom,Counter+1}),
+                    {reply, atom_to_list(Atom) ++ integer_to_list(Counter) ++ "_", State}
+	    end;
         stop ->
-            {stop, normal, State}
+	        {stop, normal, State};
+	{store, Table, Entry} ->
+	    ets:insert(Table, Entry),
+        {reply, ok, State};
+    {get, Table, Key} ->
+        [H|[]] = ets:lookup(Table, Key),
+        H
     end.
 
 handle_cast(Msg, State) ->
@@ -48,6 +93,5 @@ handle_info(Info, State) ->
 code_change(_OldVs, State, _Extra) ->
     {ok, State}.
 
-terminate(_Reason, _State) ->
+terminate(normal, _State) ->
     ok.
-    
