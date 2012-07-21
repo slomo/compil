95be02db4433e4e9106e7810079472004026bb8f
yves <steve.harrison@gmx.net>
2012-07-21 15:58:08 +0200

    improve start script, it also compiles now

--- compil.sh
+++ compil.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 
+# compile everything
+erlc *.erl
+
+# append paths
 ERL_PATHS="-pz ${BASEDIR} -pz ${BASEDIR}/lfe"
 
+# check number of parameters
 if [ $# -eq 0 ]; then
+    # start shell
     erl ${ERL_PATHS}
 else
+    # run compiler
     erl -noshell -s compil main $1 $2 -b c -s init stop ${ERL_PATHS}
 fi
 
